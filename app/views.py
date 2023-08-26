023f5ccc12262a116f82a372fcfffadd3083d1fe
HarshSonawane <sonawaneharshwardhan@gmail.com>
2023-08-26 17:02:08 +0530

    updated views

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World New Commit!")
+    q = request.GET.get('q')
+    return HttpResponse("Hello World! " + q)
